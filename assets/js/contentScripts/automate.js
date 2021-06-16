3cd3e6dd49642a5bfd089562df9e3066c8868a35
vinod1999 <vinodsasanka1999@gmail.com>
2021-06-16 22:24:09 +0530

    add new feature cursor fix.

--- assets/js/contentScripts/automate.js
+++ assets/js/contentScripts/automate.js
@@ -18,6 +18,27 @@ class Observer {
     );
     this.observer.observe(this.target, this.config);
   }
+  
+  setEndOfContenteditable(contentEditableElement) {
+    if(!(contentEditableElement && contentEditableElement instanceof HTMLElement)) return;
+
+    let range, selection;
+    if (document.createRange) {
+     
+      range = document.createRange(); //Create a range (a range is a like the selection but invisible)
+      range.selectNodeContents(contentEditableElement); //Select the entire contents of the element with the range
+      range.collapse(false); //collapse the range to the end point. false means collapse to end rather than the start
+      selection = window.getSelection(); //get the selection object (allows you to change selection)
+      selection.removeAllRanges(); //remove any selections already made
+      selection.addRange(range); //make the range you have just created the visible selection
+    } else if (document.selection) {
+      //IE 8 and lower
+      range = document.body.createTextRange(); //Create a range (a range is a like the selection but invisible)
+      range.moveToElementText(contentEditableElement); //Select the entire contents of the element with the range
+      range.collapse(false); //collapse the range to the end point. false means collapse to end rather than the start
+      range.select(); //Select the range (make it the visible selection
+    }
+  }
 
   async replacer() {
     while (true) {
@@ -133,6 +154,7 @@ class Observer {
 
         observer.disconnect();
         await this.replacer();
+        this.setEndOfContenteditable(document.querySelector('.ql-editor'))
         observer.observe(this.target, this.config);
       }
     }
